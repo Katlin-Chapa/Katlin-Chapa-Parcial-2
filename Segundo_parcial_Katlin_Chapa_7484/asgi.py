a94921f88638efca899f9050183278b4d4dcbbba
Katlin-Chapa <kchapal@miumg.edu.gt>
2024-09-28 14:11:25 -0600

    Creado

---
+++ Segundo_parcial_Katlin_Chapa_7484/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Segundo_parcial_Katlin_Chapa_7484 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Segundo_parcial_Katlin_Chapa_7484.settings')
+
+application = get_asgi_application()
